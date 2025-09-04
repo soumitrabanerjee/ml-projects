55aecc9b0c34934e16530471139dd29413d5870f
soumitrabanerjee <soumitra.banerjee@hotmail.com>
2025-09-04 13:05:31 +0530

    enhance ml_process module with error analysis and visualization; add error ratio calculations and histogram plotting

--- ml_process.py
+++ ml_process.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
+import numpy as np
 
 def ml_process(train_pandas_df, test_pandas_df):
     print(train_pandas_df.shape)
@@ -29,4 +31,22 @@ def ml_process(train_pandas_df, test_pandas_df):
     india_prediction = test_pandas_df[test_pandas_df['team'] == 'IND']
     print(india_prediction)
 
+    errors = (test_pandas_df['medals'] - test_pandas_df['predictions']).abs()
+    print(errors)
 
+    error_by_team = errors.groupby(test_pandas_df['team']).mean()
+    print(error_by_team)
+
+    medals_by_team = test_pandas_df['medals'].groupby(test_pandas_df['team']).mean()
+
+    error_ratio = error_by_team / medals_by_team
+    error_ratio = error_ratio[~pd.isnull(error_ratio)]
+    print('Error Ratio: ', error_ratio)
+
+    error_ratio = error_ratio[np.isfinite(error_ratio)]
+    print('Error Ratio after removing infinite values: \n', error_ratio)
+
+    error_ratio.plot.hist()
+    # plt.show()
+    sorted_data = error_ratio.sort_values(ascending=False)
+    print('sorted data\n', sorted_data)
